0b6128ae36bae9be14e2f09fb30b15fa490a674d
Rob Harting <robharting@hotmail.com>
2024-07-26 15:58:37 +0200

    added progress bar and finished it

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,31 +11,42 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     // via an dictonary collection
     //let eggTimes : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
     // in seconds
-    let eggTimes : [String : Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
-    var secondsRemaining = 5
-    
+    //    let eggTimes : [String : Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes : [String : Int] = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func hardnessSected(_ sender: UIButton) {
-        
+        timer.invalidate()
         let hardness: String = sender.currentTitle!
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            if self.secondsRemaining > 0 {
-                print ("\(self.secondsRemaining) seconds.")
-                self.secondsRemaining -= 1
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        labelName.text = hardness
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsPassed < self.totalTime {
+                self.secondsPassed += 1
+                self.progressBar.progress = Float(self.secondsPassed)/Float(self.totalTime)
+                
+                print ("\(self.secondsPassed) seconds passed.")
             } else {
                 Timer.invalidate()
+                self.labelName.text = "DONE"
             }
         }
         
-       
-                
-    }
         
+        
+    }
+    
     
 }
